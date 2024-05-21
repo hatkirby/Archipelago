3df9e8650cc2ee80c807e99093e8f4f2e4ab380d
Star Rauchenberger <fefferburbia@gmail.com>
2024-05-21 18:44:59 -0400

    Default to placing dungeon small keys within the dungeon

--- worlds/anodyne/__init__.py
+++ worlds/anodyne/__init__.py
@@ -1,7 +1,10 @@
+import logging
 from typing import Dict, List
 
 from BaseClasses import Tutorial, Item, Location, Region, ItemClassification
+from Fill import fill_restrictive, FillError
 from worlds.AutoWorld import WebWorld, World
+from .data.constants import ANODYNE_DUNGEONS
 from .data.items import ANODYNE_ITEMS_BY_NAME, ANODYNE_ITEMS
 from .data.locations import LocationData, ANODYNE_LOCATIONS
 from .data.logic import AnodyneConnection, get_logic
@@ -82,14 +85,20 @@ class AnodyneWorld(World):
     def create_items(self):
         pool = []
 
+        real_items = 0
         for item in ANODYNE_ITEMS:
             if item.classification not in [ItemClassification.progression, ItemClassification.useful]:
                 continue
 
+            if item.dungeon is not None and not self.options.small_key_shuffle:
+                real_items += item.amount
+                continue
+
             for i in range(0, item.amount):
                 pool.append(self.create_item(item.name))
+                real_items += 1
 
-        item_difference = len(ANODYNE_LOCATIONS) - len(pool)
+        item_difference = len(ANODYNE_LOCATIONS) - real_items
         for i in range(0, item_difference):
             pool.append(self.create_item("Heal"))
 
@@ -102,6 +111,40 @@ class AnodyneWorld(World):
 
     def set_rules(self):
         self.multiworld.completion_condition[self.player] = lambda state: state.has("Victory", self.player)
+
+    def pre_fill(self):
+        collection_state = self.multiworld.get_all_state(False)
+
+        for dungeon in ANODYNE_DUNGEONS:
+            dungeon_location_names = [location.name for location in ANODYNE_LOCATIONS if location.dungeon == dungeon]
+            dungeon_locations = [location for location in self.multiworld.get_locations(self.player)
+                                 if location.name in dungeon_location_names]
+
+            confined_dungeon_items = []
+            if not self.options.small_key_shuffle:
+                small_key_name = f"Small Key ({dungeon})"
+                small_key_item = ANODYNE_ITEMS_BY_NAME[small_key_name]
+
+                for i in range(0, small_key_item.amount):
+                    confined_dungeon_items.append(self.create_item(small_key_name))
+
+            if len(confined_dungeon_items) == 0:
+                continue
+
+            for item in confined_dungeon_items:
+                collection_state.remove(item)
+
+            for attempts_remaining in range(2, -1, -1):
+                self.random.shuffle(dungeon_locations)
+                try:
+                    fill_restrictive(self.multiworld, collection_state, dungeon_locations, confined_dungeon_items,
+
+                                     single_player_placement=True, lock=True, allow_excluded=True)
+                    break
+                except FillError as exc:
+                    if attempts_remaining == 0:
+                        raise exc
+                    logging.debug(f"Failed to shuffle dungeon items for player {self.player}. Retrying...")
 
     def get_filler_item_name(self) -> str:
         return "Heal"
--- worlds/anodyne/options.py
+++ worlds/anodyne/options.py
@@ -1,18 +1,28 @@
 from dataclasses import dataclass
 
-from Options import PerGameCommonOptions, Range, StartInventoryPool
+from Options import PerGameCommonOptions, Range, StartInventoryPool, Toggle
 
 
 class EndgameCardRequirement(Range):
-    """The number of cards needed to open the gate that leads to the endgame area.
-    Setting this to zero removes the gate."""
+    """
+    The number of cards needed to open the gate that leads to the endgame area.
+    Setting this to zero removes the gate.
+    """
     display_name = "Terminal Card Requirement"
     range_start = 0
     range_end = 37
     default = 36
 
 
+class SmallKeyShuffle(Toggle):
+    """
+    If enabled, small keys will be shuffled everywhere, instead of being confined to their own dungeons.
+    """
+    display_name = "Small Key Shuffle"
+
+
 @dataclass
 class AnodyneOptions(PerGameCommonOptions):
     endgame_card_requirement: EndgameCardRequirement
+    small_key_shuffle: SmallKeyShuffle
     start_inventory_from_pool: StartInventoryPool
